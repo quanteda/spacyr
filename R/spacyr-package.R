b9c4159228fc10fde597f02ea71651da6d0baacb
Kenneth Benoit <kbenoit@lse.ac.uk>
2016-11-10 20:15:12 -0500

    Add benchmarks/ folder Add spacyr-package Rd

---
+++ R/spacyr-package.R
@@ -0,0 +1,9 @@
+#' An R wrapper to the spaCy NLP system
+#'
+#' @author Ken Benoit and Akitaka Matsuo
+#' 
+#' An R wrapper to the Python (Cython) spaCy NLP system, 
+#' from http://spacy.io.  Nicely integrated with \pkg{quanteda}.
+"_PACKAGE"
+
+
