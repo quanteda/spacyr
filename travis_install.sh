2c27963265e6fbc42a9930595c7dc8ebd0dc71a4
amatsuo <matsuoakitaka@gmail.com>
2016-11-14 11:32:08 +0000

    attempt travis build fix (#5)

--- travis_install.sh
+++ travis_install.sh
@@ -8,4 +8,4 @@ source testenv/bin/activate
 # Python dependencies
 sudo pip install --upgrade pip
 sudo pip install spacy
-sudo python -m spacy.en.download all
+sudo python -m spacy.en.download all > dump.log
