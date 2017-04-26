673a370fd8487ef4e3b225d342132f6c76077a16
amatsuo <matsuoakitaka@gmail.com>
2017-04-26 16:59:50 +0100

    change the way python path is set

--- R/package.R
+++ R/package.R
@@ -5,11 +5,11 @@
 #' @name spacyr
 NULL
 
-
-common_obj <- new.env(parent=emptyenv())
-
 .onLoad <- function(libname, pkgname) {
     # setting up the reticulate python part is moved to spacy_initialize()
+    # clear options
+    options("spacy_initialized" = NULL)
+    options("python_initialized" = NULL)
 }
 
 
--- R/spacy_initialize.R
+++ R/spacy_initialize.R
@@ -1,16 +1,48 @@
 #' Initialize spaCy
 #' 
-#' Initialize spaCy to call from R.
+#' Initialize spaCy to call from R. 
 #' @return NULL
 #' @param lang Language package for loading spacy. Either \code{en} (English) or 
 #' \code{de} (German). Default is \code{en}.
+#' @param use_python set a path to the python excutable with spaCy. 
+#' @param use_virtualenv set a path to the python virtual environment with spaCy. 
+#'   Example: \code{use_virtualenv = "~/myenv"}
+#' @param use_condaenv set a path to the anaconda virtual environment with spaCy. 
+#'   Example: \code{use_condalenv = "myenv"}
 #' @export
 #' @author Akitaka Matsuo
-spacy_initialize <- function(lang = 'en') {
-    # if SPACY_PYTHON is defined then forward it to RETICULATE_PYTHON
-    spacy_python <- Sys.getenv("SPACY_PYTHON", unset = NA)
-    if (!is.na(spacy_python))
-        Sys.setenv(RETICULATE_PYTHON = spacy_python)
+spacy_initialize <- function(lang = 'en', 
+                             use_python = NA,
+                             use_virtualenv = NA,
+                             use_condaenv = NA) {
+    # here are a number of checkings
+    if(!is.null(options("spacy_initialized")$spacy_initialized)){
+        message("spacy is already initialized")
+        return(NULL)
+    }
+    # once python is initialized, you cannot change the python executables
+    if(!is.null(options("python_initialized")$python_initialized)) {
+        message("python is already initialized, following arguments are ignored: \n\tuse_python, use_virtualenv, use_condaenv")
+    } 
+    # a user can specify only one
+    else if(sum(!is.na(c(use_python, use_virtualenv, use_condaenv))) > 1) {
+        stop(paste("Too many python environments are specified, please select only one",
+                   "from use_python, use_virtualenv, and use_condaenv"))
+    }
+    # give warning when nothing is specified
+    else if (sum(!is.na(c(use_python, use_virtualenv, use_condaenv))) == 0){
+        def_python <- ifelse(Sys.info()['sysname'] == "Windows", 
+                             system("where python", intern = TRUE), 
+                             system("which python", intern = TRUE))
+        message(paste("No python executable is specified, spacyr will use system default python\n",
+                       sprintf("(system default python: %s).", def_python)))
+    } 
+    else {# set the path with reticulte
+        if(!is.na(use_python)) reticulate::use_python(use_python, required = TRUE)
+        else if(!is.na(use_virtualenv)) reticulate::use_virtualenv(use_virtualenv, required = TRUE)
+        else if(!is.na(use_condaenv)) reticulate::use_condaenv(use_condaenv, required = TRUE)
+    }
+    options("python_initialized" = TRUE) # next line could cause non-recoverable error 
     spacyr_pyexec(pyfile = system.file("python", "spacyr_class.py",
                                        package = 'spacyr'))
     if(! lang %in% c('en', 'de')) {
@@ -19,6 +51,7 @@ spacy_initialize <- function(lang = 'en') {
     spacyr_pyassign("lang", lang)
     spacyr_pyexec(pyfile = system.file("python", "initialize_spacyPython.py",
                                        package = 'spacyr'))
+    message("spacy is successfully initialized")
     options("spacy_initialized" = TRUE)
 }
 
