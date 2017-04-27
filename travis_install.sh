afb6d001290ad515eefe82c2dff2f2f67fc0c56f
amatsuo <matsuoakitaka@gmail.com>
2017-04-27 18:32:40 +0100

    try travis fix

--- travis_install.sh
+++ travis_install.sh
@@ -2,12 +2,11 @@
 
 # create virtualenv
 deactivate
-virtualenv --system-site-packages testenv
+virtualenv -p /usr/bin/python2.7 --system-site-packages env
 source testenv/bin/activate
 
 # Python dependencies
-sudo pip install --upgrade pip
-sudo pip install requests[security]
-sudo pip install spacy
-sudo python -m spacy download en
+pip install --upgrade pip
+pip install spacy
+python -m spacy download en
 
