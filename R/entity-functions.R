a87117077da2d8aa93e63ec8c179b877527a71c5
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-05-13 10:33:36 +0100

    Modify entity extraction functions
    - Change type for entity_consolidate()
    - Add concatenator for entity_consolidate()
    - Remove dependency info entirely when consolidating entities with dependency parse info

--- R/entity-functions.R
+++ R/entity-functions.R
@@ -27,8 +27,8 @@ entity_extract <- function(spacy_result, type = c("named", "extended", "all")) {
     
     type <- match.arg(type)
     
-    if(!"entity" %in% names(spacy_result)) {
-        stop("Entity Recognition is not conducted\nNeed to rerun spacy_parse() with entity = TRUE") 
+    if (!"entity" %in% names(spacy_result)) {
+        stop("no entities in parsed object: rerun spacy_parse() with entity = TRUE") 
     }
     spacy_result <- spacy_result[nchar(spacy_result$entity) > 0]
     spacy_result[, entity_type := sub("_.+", "", entity)]
@@ -41,7 +41,7 @@ entity_extract <- function(spacy_result, type = c("named", "extended", "all")) {
                                       .SDcols = c("tokens")]$tokens] 
     extended_list <- c("DATE", "TIME", "PERCENT", "MONEY", "QUANTITY", "ORDINAL", 
                        "CARDINAL")
-    if(type == 'extended'){
+    if (type == 'extended'){
         entities <- entities[entity_type %in% extended_list]
     } else if (type == 'named') {
         entities <- entities[!entity_type %in% extended_list]
@@ -55,41 +55,38 @@ entity_extract <- function(spacy_result, type = c("named", "extended", "all")) {
 #' \code{entity_consolidate} concatinate named entity in a table of
 #' results of \code{spacy_parse}
 #' @param spacy_result a \code{data.frame} from \code{\link{spacy_parse}}.
-#' @param type type of named entities to be consolidated, either \code{named}, \code{extended}, or
-#'   \code{all}.  See 
-#'   \url{https://spacy.io/docs/usage/entity-recognition#entity-types} for
-#'   details.
-#' @return A modified \code{data.table} of spacy outputs.
+#' @param concatenator the character used to concatenator elements of multi-word named entities 
+#' @return A modified \code{data.frame} of spacy outputs, where the named entities have been 
+#' combined into a single "token".
 #' @importFrom data.table data.table
 #' @examples
 #' \donttest{
 #' spacy_initialize()
 #' 
-#' parsed_sentences <- spacy_parse(data_char_sentences, entity = TRUE, dependency = TRUE)
+#' parsed_sentences <- spacy_parse(data_char_sentences, entity = TRUE)
 #' parsed_sentences_consolidated <- entity_consolidate(parsed_sentences)
 #' head(parsed_sentences_consolidated, 30)
 #' }
 #' @export
-entity_consolidate <- function(spacy_result, type = c("named", "extended", "all")) {
+entity_consolidate <- function(spacy_result, concatenator = "_") {
     
-    entity_type <- entity <- iob <- entity_id <- .N <- .SD <- `:=` <- sentence_id <- docname <- NULL
+    entity <- entity_type <- entity_count <- iob <- entity_id <- .N <- .SD <- `:=` <- 
+        lemma <- pos <- tag <- new_token_id <- token_id <- sentence_id <- docname <- NULL
     
-    type <- match.arg(type)
-
-    if(!"entity" %in% names(spacy_result)) {
-        stop("Entity Recognition is not conducted\nNeed to rerun spacy_parse() with entity = TRUE") 
+    if (!"entity" %in% names(spacy_result)) {
+        stop("no entities in parsed object: rerun spacy_parse() with entity = TRUE") 
     }
     spacy_result[, entity_type := sub("_.+", "", entity)]
     spacy_result[, iob := sub(".+_", "", entity)]
     extended_list <- c("DATE", "TIME", "PERCENT", "MONEY", "QUANTITY", "ORDINAL",
                        "CARDINAL")
-    if(type == 'extended'){
-        spacy_result[entity_type != ""  & !(entity_type %in% extended_list),
-                     c("entity_type", "iob") := ""]
-    } else if (type == 'named') {
-        spacy_result[entity_type != ""  & (entity_type %in% extended_list),
-                     c("entity_type", "iob") := ""]
-    }
+    # if (type == 'extended'){
+    #     spacy_result[entity_type != ""  & !(entity_type %in% extended_list),
+    #                  c("entity_type", "iob") := ""]
+    # } else if (type == 'named') {
+    #     spacy_result[entity_type != ""  & (entity_type %in% extended_list),
+    #                  c("entity_type", "iob") := ""]
+    # }
     spacy_result[, entity_count := ifelse(iob=="B"|iob == "", 1, 0)]
     spacy_result[, entity_id := cumsum(entity_count), by = c("docname", "sentence_id")]
     spacy_result_modified <- spacy_result[, lapply(.SD, function(x) x[1]), 
@@ -98,44 +95,45 @@ entity_consolidate <- function(spacy_result, type = c("named", "extended", "all"
                                                             c("docname", "sentence_id", "entity_id"))]
     
     spacy_result_modified[
-        , tokens := spacy_result[, lapply(.SD, function(x) paste(x, collapse = "_")), 
+        , tokens := spacy_result[, lapply(.SD, function(x) paste(x, collapse = concatenator)), 
                                  by = c("docname", "sentence_id", "entity_id"), 
                                  .SDcols = "tokens"]$tokens] 
-    if("lemma" %in% colnames(spacy_result)) {
+    
+    if ("lemma" %in% colnames(spacy_result)) {
         spacy_result_modified[
             , lemma := spacy_result[, lapply(.SD, function(x) paste(x, collapse = "_")), 
                                     by = c("docname", "sentence_id", "entity_id"), 
                                     .SDcols = "lemma"]$lemma] 
         
     }
-    if("pos" %in% names(spacy_result_modified)){
+    if ("pos" %in% names(spacy_result_modified)){
         spacy_result_modified[nchar(entity_type) > 0, pos := "ENTITY"]
     }
-    if("tag" %in% names(spacy_result_modified)){
+    if ("tag" %in% names(spacy_result_modified)){
         spacy_result_modified[nchar(entity_type) > 0, tag := "ENTITY"]
     }
     spacy_result_modified[, new_token_id := entity_id]
     
-    if("dep_rel" %in% names(spacy_result)) {
-        message("Note: head_token_id, dep_rel for named entities will be converted to NA")
-        dt_id_match <- spacy_result[, .(docname, sentence_id, token_id, entity_id)]
-        data.table::setnames(dt_id_match, "token_id", "head_token_id")
-        data.table::setnames(dt_id_match, "entity_id", "new_head_token_id")
-        #data.table::set2keyv(dt_id_match, "head_token_id")
-        spacy_result_modified[, serialn := seq(nrow(spacy_result_modified))]
-        #data.table::set2keyv(spacy_result_modified, "head_token_id")
-        browser()
-        spacy_result_modified <- merge(spacy_result_modified, dt_id_match, 
-                                       by = c("docname", "sentence_id", "head_token_id"), all.x = TRUE)
-        spacy_result_modified <- spacy_result_modified[order(serialn)]
-        spacy_result_modified[, head_token_id := NULL]
-        data.table::setnames(spacy_result_modified, "new_head_token_id", "head_token_id")
-        spacy_result_modified[nchar(entity_type) > 0, head_token_id := NA]
+    # for now, just obliterate dependency parsing for consolidated NEs
+    if ("dep_rel" %in% names(spacy_result_modified)){
+        message("Note: removing head_token_id, dep_rel for named entities")
+        spacy_result_modified[, c("dep_rel", "head_token_id") := NULL]
     }
-    if("dep_rel" %in% names(spacy_result_modified)){
-        spacy_result_modified[nchar(entity_type) > 0, dep_rel := NA]
-    }
-    
+        
+    #     dt_id_match <- spacy_result[, .(docname, sentence_id, token_id, entity_id)]
+    #     data.table::setnames(dt_id_match, "token_id", "head_token_id")
+    #     data.table::setnames(dt_id_match, "entity_id", "new_head_token_id")
+    #     #data.table::set2keyv(dt_id_match, "head_token_id")
+    #     spacy_result_modified[, serialn := seq(nrow(spacy_result_modified))]
+    #     #data.table::set2keyv(spacy_result_modified, "head_token_id")
+    #     spacy_result_modified <- merge(spacy_result_modified, dt_id_match, 
+    #                                    by = c("docname", "sentence_id", "head_token_id"), all.x = TRUE)
+    #     spacy_result_modified <- spacy_result_modified[order(serialn)]
+    #     spacy_result_modified[, head_token_id := NULL]
+    #     data.table::setnames(spacy_result_modified, "new_head_token_id", "head_token_id")
+    #     spacy_result_modified[nchar(entity_type) > 0, head_token_id := NA]
+    # }
+
     spacy_result_modified[, token_id := NULL]
     data.table::setnames(spacy_result_modified, "new_token_id", "token_id")
     keep_cols <- intersect(c("docname", "sentence_id", "token_id", "tokens", 
