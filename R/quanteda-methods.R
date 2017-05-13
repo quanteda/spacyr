15f34737095e95ed9612442381363ed9ab4a919a
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-05-13 13:18:13 +0100

    Update quanteda methods for data.frame objects

--- R/quanteda-methods.R
+++ R/quanteda-methods.R
@@ -23,7 +23,7 @@
 #' require(quanteda)
 #' spacy_initialize()
 #' txt <- c(doc1 = "And now, now, now for something completely different.",
-#'          doc2 = "Jack and Jill ran up the hill.")
+#'          doc2 = "Jack and Jill are children.")
 #' parsed <- spacy_parse(txt)
 #' ntype(parsed)
 #' ntoken(parsed)
@@ -41,8 +41,7 @@ NULL
 #' @noRd
 #' @export
 docnames.spacyr_parsed <- function(x) {
-    docname <- NULL
-    unique(x[, docname])
+    unique(x$docname)
 }
 
 
@@ -63,15 +62,11 @@ ndoc.spacyr_parsed <- function(x) {
 #' \code{ntoken} returns the number of tokens by document
 #' 
 #' @importFrom quanteda ntoken
-#' @importFrom data.table data.table
 #' @method ntoken spacyr_parsed
 #' @noRd
 #' @export
 ntoken.spacyr_parsed <- function(x, ...) {
-    N <- .N <- docname <- NULL
-    ret <- x[, .N, by = docname][, N]
-    names(ret) <- docnames(x)
-    ret
+    lengths(split(parsed$token, parsed$docname))
 }
 
 #' @rdname quanteda-methods
@@ -79,13 +74,11 @@ ntoken.spacyr_parsed <- function(x, ...) {
 #' \code{ntype} returns the number of types (unique tokens) by document
 #' 
 #' @importFrom quanteda ntype
-#' @importFrom data.table data.table
 #' @method ntype spacyr_parsed
 #' @noRd
 #' @export
 ntype.spacyr_parsed <- function(x, ...) {
-    docname <- token <- .N <- NULL
-    ntoken(x[, .N, by = list(docname, token)])
+    sapply(split(parsed$token, parsed$docname), function(y) length(unique(y)))
 }
 
 # print a tokenizedTexts objects
@@ -98,38 +91,38 @@ ntype.spacyr_parsed <- function(x, ...) {
 # @export
 # @keywords internal
 # @method print tokenizedTexts_tagged
-print.tokenizedTexts_tagged <- function(x, sep = "_", ...) {
-    ndocuments <- ifelse(is.list(x), length(x), 1)
-    if( "token" %in% class(x)) {
-        x <- as.tokenizedTexts(x)
-        class(x) <- c("tokenizedTexts_tagged", class(x))   
-    }
-    cat("tokenizedText_tagged object from ", ndocuments, " document", 
-        ifelse(ndocuments > 1, "s", ""), 
-        " (tagset = ", attr(x, "tagset"), ").\n", 
-        sep = "")
-    
-    if (!is.null(sep)) {
-        
-        docs <- factor(rep(docnames(x), times = ntoken(x)), levels = docnames(x))
-        tmp <- split(paste(unlist(x), unlist(attr(x, "tags")), sep = sep),  docs)
-        class(tmp) <- "listof"
-        print(tmp)
-        
-    } else {
-        
-        for (e in docnames(x)) {
-            cat(paste0(e, ":\n"))
-            if (is.list(x[[tolower(e)]])) { 
-                class(x[[tolower(e)]]) <- "listof"
-                print(x[[tolower(e)]], ...)
-            } else {
-                print(as.character(x[[tolower(e)]]), ...)
-            }
-        }
-        
-    }
-}
+# print.tokenizedTexts_tagged <- function(x, sep = "_", ...) {
+#     ndocuments <- ifelse(is.list(x), length(x), 1)
+#     if( "token" %in% class(x)) {
+#         x <- as.tokenizedTexts(x)
+#         class(x) <- c("tokenizedTexts_tagged", class(x))   
+#     }
+#     cat("tokenizedText_tagged object from ", ndocuments, " document", 
+#         ifelse(ndocuments > 1, "s", ""), 
+#         " (tagset = ", attr(x, "tagset"), ").\n", 
+#         sep = "")
+#     
+#     if (!is.null(sep)) {
+#         
+#         docs <- factor(rep(docnames(x), times = ntoken(x)), levels = docnames(x))
+#         tmp <- split(paste(unlist(x), unlist(attr(x, "tags")), sep = sep),  docs)
+#         class(tmp) <- "listof"
+#         print(tmp)
+#         
+#     } else {
+#         
+#         for (e in docnames(x)) {
+#             cat(paste0(e, ":\n"))
+#             if (is.list(x[[tolower(e)]])) { 
+#                 class(x[[tolower(e)]]) <- "listof"
+#                 print(x[[tolower(e)]], ...)
+#             } else {
+#                 print(as.character(x[[tolower(e)]]), ...)
+#             }
+#         }
+#         
+#     }
+# }
 
 
 # summarize a tagged tokenizedTexts object
@@ -140,13 +133,14 @@ print.tokenizedTexts_tagged <- function(x, sep = "_", ...) {
 # @importFrom data.table rbindlist
 # @export
 # @method summary spacyr_parsed
-summary.spacyr_parsed <- function(object, ...) {
-    result <- data.frame(
-        data.table::rbindlist(lapply(attr(object, "tags"), function(doc) as.list(table(doc))), 
-                              use.names = TRUE, fill = TRUE)
-    )
-    result[is.na(result)] <- 0
-    row.names(result) <- docnames(object)
-    
-    result
-}
+# summary.spacyr_parsed <- function(object, ...) {
+#     object <- data.table(object)
+#     result <- data.frame(
+#         data.table::rbindlist(lapply(attr(object, "tags"), function(doc) as.list(table(doc))), 
+#                               use.names = TRUE, fill = TRUE)
+#     )
+#     result[is.na(result)] <- 0
+#     row.names(result) <- docnames(object)
+#     
+#     result
+# }
---
+++ tests/testthat/test-quanteda-methods.R
@@ -0,0 +1,36 @@
+context("test quanteda functions")
+
+test_that("quanteda functions work", {
+    spacy_initialize()
+    
+    txt <- c(doc1 = "And now, now, now for something completely different.",
+             doc2 = "Jack and Jill are children.")
+    parsed <- spacy_parse(txt)
+    
+    expect_equal(
+        ntype(parsed),
+        c(doc1 = 8, doc2 = 6)
+    )
+    
+    expect_equal(
+        ntoken(parsed),
+        c(doc1 = 11, doc2 = 6)
+    )
+    
+    expect_equal(
+        ntype(parsed),
+        c(doc1 = 8, doc2 = 6)
+    )
+    
+    expect_equal(
+        ndoc(parsed),
+        2
+    )
+    
+    expect_equal(
+        docnames(parsed),
+        c("doc1", "doc2")
+    )
+
+    spacy_finalize()
+})
