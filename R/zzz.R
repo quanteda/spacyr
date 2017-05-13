7022db2bde8086a2c1926cce2eedead93d8e677a
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-05-13 09:18:50 +0100

    Add .onUnload, move to zzz.R
    Also remove redundant second package help file.

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,3 @@
-#' Spacy for R
-#'
-#'
-#' @docType package
-#' @name spacyr
-#' @rdname spacyr-package
-NULL
-
 .onLoad <- function(libname, pkgname) {
     # setting up the reticulate python part is moved to spacy_initialize()
     # clear options
@@ -13,4 +5,7 @@ NULL
     options("python_initialized" = NULL)
 }
 
+.onUnload <- function(libpath) {
+    spacy_finalize()
+}
 
