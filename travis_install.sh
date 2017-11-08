cd668b469655e35da920a48d36f6b4fff378fbf7
amatsuo <matsuoakitaka@gmail.com>
2017-11-08 16:16:54 +0000

    add numpy

--- travis_install.sh
+++ travis_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # create virtualenv
-# deactivate
+deactivate
 virtualenv -p /usr/bin/python3 --system-site-packages testenv
 source testenv/bin/activate
 
 # Python dependencies
 pip install --upgrade pip
 pip install --upgrade html5lib 
-pip install spacy
+pip install -U spacy
 python -m spacy download en
