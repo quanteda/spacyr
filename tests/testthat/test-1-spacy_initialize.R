7b767b22dacd2a3c424e5e64365576c622259277
amatsuo <matsuoakitaka@gmail.com>
2018-01-14 21:36:02 +0900

    skip if python not available

--- tests/testthat/test-1-spacy_initialize.R
+++ tests/testthat/test-1-spacy_initialize.R
@@ -1,6 +1,8 @@
 context("test spacy_initialize")
 
 test_that("spacy_initialize() with non-existent python (#49)", {
+    if (!reticulate::py_available(initialize = TRUE))
+        skip("Python bindings not available for testing")
     expect_error(
         spacy_initialize(python_executable = "/usr/local/bin/notpython"),
         "notpython is not a python executable"
