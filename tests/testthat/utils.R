48a58ff6d9e54f0025698cd69b655400f146a2d8
amatsuo <matsuoakitaka@gmail.com>
2018-03-23 14:44:00 +0000

    update a function in test

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -5,7 +5,7 @@ skip_if_no_python_or_no_spacy <- function() {
         source_bash_profile <- TRUE
     }
     if(find_spacy_env()) return(NULL)
-    spacy_path <- find_spacy(ask = FALSE, source_bash_profile = source_bash_profile)
+    spacy_path <- find_spacy(ask = FALSE)
     if(is.null(spacy_path)) {
         skip("Skip the test as spaCy is not found")
     } else if (is.na(spacy_path)) {
