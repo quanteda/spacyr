0dacd1e4ee2754b0ea9fd66ad789dc1e414a2821
amatsuo <matsuoakitaka@gmail.com>
2018-05-18 13:13:25 +0100

    compatibility with pip 10

--- inst/python/initialize_spacyPython.py
+++ inst/python/initialize_spacyPython.py
@@ -1,8 +1,14 @@
 # from __future__ import unicode_literals 
 
 ## following lines are necessary for showing the version of spacy
-import pip
-installed_packages = pip.get_installed_distributions()
+## Dealing with pip 10.* api chagnes
+## The solution is from:
+## https://github.com/naiquevin/pipdeptree/blob/master/pipdeptree.py
+try:
+    from pip._internal import get_installed_distributions
+except ImportError:
+    from pip import get_installed_distributions
+installed_packages = get_installed_distributions()
 versions = {package.key: package.version for package in installed_packages}
 
 if 'spacy_entity' in locals() and spacy_entity == False and int(versions['spacy'][:1]) >= 2:
