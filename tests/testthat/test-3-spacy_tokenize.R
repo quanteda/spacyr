fb7e6423fa66355cf4a3bb4da64efa7a4951783c
amatsuo <matsuoakitaka@gmail.com>
2018-10-24 19:25:01 +0100

    insert skip_on lines in tokenize tests

--- tests/testthat/test-3-spacy_tokenize.R
+++ tests/testthat/test-3-spacy_tokenize.R
@@ -2,6 +2,10 @@ context("test spacy_parse")
 source("utils.R")
 
 test_that("spacy_tokenize docnames work as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- "This is a test for document names."
     expect_identical(
         names(spacy_tokenize(txt)), "text1"
@@ -17,6 +21,10 @@ test_that("spacy_tokenize docnames work as expected", {
 })
 
 test_that("spacy_tokenize remove_punct argument work as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- "This: £ = GBP! 15% not! > 20 percent?"
     expect_equivalent(
         spacy_tokenize(txt, remove_punct = FALSE), 
@@ -29,6 +37,10 @@ test_that("spacy_tokenize remove_punct argument work as expected", {
 })
 
 test_that("spacy_tokenize padding argument work as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- "This: a test."
     expect_equivalent(
         spacy_tokenize(txt, remove_punct = FALSE, padding = TRUE), 
@@ -45,6 +57,10 @@ test_that("spacy_tokenize padding argument work as expected", {
 })
 
 test_that("spacy_tokenize remove_punct works as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- "My favorite: the very! nice? ±2 for €5 beers."
     expect_equivalent(
         spacy_tokenize(txt, remove_punct = TRUE, padding = FALSE),
@@ -53,6 +69,10 @@ test_that("spacy_tokenize remove_punct works as expected", {
 })
 
 test_that("spacy_tokenize remove_url works as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- c(doc1 = "test@unicode.org can be seen at https://bit.ly/2RDxcxs?not=FALSE.")
     expect_equivalent(
         spacy_tokenize(txt, remove_url = FALSE, padding = FALSE, remove_punct = FALSE),
@@ -69,6 +89,10 @@ test_that("spacy_tokenize remove_url works as expected", {
 })
 
 test_that("spacy_tokenize remove_numbers works as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- c(doc1 = "99 red ballons 4ever £5 gr8!!")
     expect_equivalent(
         spacy_tokenize(txt, remove_numbers = FALSE, padding = FALSE),
@@ -85,6 +109,10 @@ test_that("spacy_tokenize remove_numbers works as expected", {
 })
 
 test_that("spacy_tokenize remove_separators works as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- c(doc1 = "Sentence  one\ttwo\nNew paragraph\u2029Last paragraph")
     expect_equivalent(
         spacy_tokenize(txt, remove_separators = FALSE),
@@ -101,6 +129,10 @@ test_that("spacy_tokenize remove_separators works as expected", {
 })
 
 test_that("spacy_tokenize multithread = TRUE does not change value", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     expect_identical(
         spacy_tokenize(data_char_paragraph, multithread = TRUE),
         spacy_tokenize(data_char_paragraph, multithread = FALSE)
@@ -108,6 +140,10 @@ test_that("spacy_tokenize multithread = TRUE does not change value", {
 })
 
 test_that("spacy_tokenize multithread = TRUE is faster than when FALSE", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     skip("multithread = TRUE performance test skipped because takes so long")
     txt <- rep(data_char_paragraph, 5000)
     expect_lt(
@@ -117,6 +153,10 @@ test_that("spacy_tokenize multithread = TRUE is faster than when FALSE", {
 })
 
 test_that("spacy_tokenize what = 'sentence' works as expected", {
+    skip_on_cran()
+    skip_on_os("solaris")
+    skip_if_no_python_or_no_spacy()
+    
     txt <- "Sentence one!  This: is a test.\n\nYeah, right.  What, Mr. Jones?"
     expect_equivalent(
         spacy_tokenize(txt, what = "sentence", remove_punct = TRUE,
