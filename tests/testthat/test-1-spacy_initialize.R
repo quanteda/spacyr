b2b692ab09fd292774ee4a70ad80d63359c0de1a
amatsuo <matsuoakitaka@gmail.com>
2018-11-30 12:22:55 +0000

    fix test

--- tests/testthat/test-1-spacy_initialize.R
+++ tests/testthat/test-1-spacy_initialize.R
@@ -4,7 +4,8 @@ source("utils.R")
 test_that("spacy_initialize() with non-existent python (#49)", {
     skip_on_os("solaris")
     expect_error(
-        spacy_initialize(python_executable = "/usr/local/bin/notpython", check_env = FALSE),
+        spacy_initialize(python_executable = "/usr/local/bin/notpython", check_env = FALSE,
+                         refresh_settings = TRUE),
         "notpython is not a python executable"
     )
 })
@@ -15,7 +16,7 @@ test_that("spacy_initialize works as expected", {
     skip_if_no_python_or_no_spacy()
     
     options(spacy_prompt = FALSE)
-    expect_message(spacy_initialize(refresh_settings = TRUE, save_profile = TRUE), 
+    expect_message(spacy_initialize(refresh_settings = TRUE), 
                    "successfully")
     
     spacy_finalize()
